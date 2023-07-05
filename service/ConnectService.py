78fde787f023353fd470a43b58a1b97013578d65
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2023-07-04 21:31:36 -0500

    switched to qnetwork and it's requests!

--- service/ConnectService.py
+++ service/ConnectService.py
@@ -4,14 +4,19 @@
 # DO NOT use this since it barely does anything
 # so you can implement your OWN service!
 
-import requests, random
-from PySide6.QtCore import QThread, QObject, Signal
+import requests, random, base64
+from PySide6.QtNetwork import *
+from PySide6.QtCore import *
 from service import DataManager
 
 language = DataManager.setMan.currentLanguage
 
 class ConnectionService(DataManager.OutputClass):
+    def __init__(self):
+        self.netManager = QNetworkAccessManager()
+
     def testInternet(self):
+        #This request is using the requests synchronous library only because its a test connection and i don't want the user to do anything in that moment.
         try:
             requests.head('https://8.8.8.8', timeout=5) #pinging google and only the head so i don't run out of resources by loading the html
             return True
@@ -24,19 +29,24 @@ class ConnectionService(DataManager.OutputClass):
         state.statusLabel.setText(language.getLanString("waiting"))
         state.statusBar.setValue(20)
         try:
+            r = QNetworkRequest(QUrl('https://httpbin.org/basic-auth/' + user + "/" + passw))
             state.statusLabel.setText(language.getLanString("attempting"))
             state.statusBar.setValue(50)
+            basicdata = user + ":" + passw
             if ran > 1:
-                c = requests.get('https://httpbin.org/basic-auth/' + user + '/' + passw, auth=(user, passw))
-            else:
-                c = requests.get('https://httpbin.org/basic-auth/' + user + '/' + passw, auth=('nuh', 'uh'))
-            state.statusLabel.setText(language.getLanString("verificating"))
-            state.statusBar.setValue(80)
-            return c.status_code
+                r.setRawHeader(b'Authorization', b'Basic ' + base64.b64encode(basicdata.encode()))
+                ans = self.netManager.get(r)
+                loadLoop = QEventLoop()
+                ans.finished.connect(loadLoop.quit)
+                loadLoop.exec_()
+            else: # same request, no headers!
+                ans = self.netManager.get(r)
+                loadLoop = QEventLoop()
+                ans.finished.connect(loadLoop.quit)
+                loadLoop.exec_()
+            return ans.attribute(QNetworkRequest.Attribute.HttpStatusCodeAttribute)
         except Exception as err:
             state.generateConError(str(err))
-        finally:
-            c.close()
 
 conService = ConnectionService()
 
